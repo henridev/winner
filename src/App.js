48e940e9a406fda36c4fe99351bf42a047719e51
henridev <henri.dbel@gmail.com>
2020-04-13 19:15:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AngularIcon from "./assets/angular.svg";
+import ReactIcon from "./assets/react.svg";
+import VueIcon from "./assets/vue.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={AngularIcon} className="App-logo" alt="logo" />
+      <img src={ReactIcon} className="App-spin App-logo" alt="logo" />
+      <img src={VueIcon} className="App-bounce App-logo" alt="logo" />
     </div>
   );
 }
